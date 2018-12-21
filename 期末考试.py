de460c07e6345aa514e31489890a5f75adb16644
cxm1207 <45995071+cxm1207@users.noreply.github.com>
2018-12-21 18:19:44 +0800

    Add files via upload

---
+++ 期末考试.py
@@ -0,0 +1,38 @@
+Name = input("你的名字是：")
+Grade = input("你的年级是（填数字）：")
+print(Name+",祝贺你考100分")
+print("|     100分     |")
+print("|_______________|")
+print("|_______________|")
+print("|               |")
+print("|    Y(^o^)Y    |")
+
+if Grade=1:
+    print(""
+          "")
+    print("PS：说真的一年级考100很应该")
+
+if Grade=2:
+    print(""
+          "")
+    print("PS：二年级，嗯……不错")
+
+if Grade=3:
+    print(""
+          "")
+    print("PS：三年级考100很不错嘛")
+
+if Grade=4:
+    print(""
+          "")
+    print("PS：四年级考100？！是尖子生吧")
+
+if Grade=5:
+    print(""
+          "")
+    print("PS：五年级！！！真厉害")
+
+if Grade=6:
+    print(""
+          "")
+    print("PS：神童，清收下我的双膝")
